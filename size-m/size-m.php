f89ecb1634e984f852cf2d661b335d0d23d9246a
MattBlackPizza <45101881+MattBlackPizza@users.noreply.github.com>
2019-07-12 12:19:11 +0200

    Add files via upload

---
+++ size-m/size-m.php
@@ -0,0 +1,104 @@
+<p class="title-section">Nav Timeline</p>
+<!--- Nav TimeLine -->
+<section class="stadium-size-m size-m">
+  <?php include 'nav-timeline.php'; ?>
+  <?php include 'player-line-up-content.php'; ?>
+  <?php include 'menu.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">Nav Classique</p>
+<!--- Nav Classique -->
+<section class="stadium-size-m size-m size-m-view">
+  <?php include 'nav.php'; ?>
+  <?php include 'player-line-up-content.php'; ?>
+  <?php include 'menu.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">Front View</p>
+<!--- Nav Classique -->
+<section class="stadium-size-m-front-view size-m size-m-view">
+  <?php include 'nav-timeline.php'; ?>
+  <?php include 'menu.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">2D View</p>
+<!--- Front View -->
+<section class="stadium-size-m-2d size-m size-m-view">
+  <?php include 'nav.php'; ?>
+  <?php include 'menu.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">Stats</p>
+<!--- Stats -->
+<section class="stadium-size-m size-m">
+  <?php include 'stats.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
+
+<p class="title-section">Match Sheet</p>
+<!--- Match Sheet -->
+<section class="stadium-size-m size-m">
+  <?php include 'match-sheet.php'; ?>
+</section>
+
+<p class="title-section">Line Up</p>
+<!--- Line Up -->
+<section class="stadium-size-m size-m">
+  <?php include 'line-up.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
+
+<p class="title-section">Notification</p>
+<!--- Notification(s) -->
+<section class="stadium-size-m size-m">
+  <?php include 'menu.php'; ?>
+  <?php include 'notification.php'; ?>
+  <?php include 'nav.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">Alert Message</p>
+<!--- Message Alert -->
+<section class="stadium-size-m size-m">
+  <?php include 'nav.php'; ?>
+  <?php include 'alert.php'; ?>
+  <?php include 'menu.php'; ?>
+  <?php include 'match-infos.php'; ?>
+</section>
+
+<p class="title-section">Match Infos</p>
+<!--- Match-infos -->
+<section class="animated stadium-size-m size-m">
+  <?php include 'nav.php'; ?>
+  <?php include 'match-infos.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
+
+<p class="title-section">Ground</p>
+<!--- Ground -->
+<section class="animated ground-size-m size-m">
+  <?php include 'nav.php'; ?>
+  <?php include 'player-line-up-content.php'; ?>
+  <?php include 'match-infos.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
+
+<p class="title-section">Ground 2D</p>
+<!--- Ground 2D View -->
+<section class="animated ground-size-m-2d size-m">
+  <?php include 'nav.php'; ?>
+  <?php include 'match-infos.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
+
+<p class="title-section">Ground Front View</p>
+<!--- Ground Front View -->
+<section class="animated ground-size-m-front-view size-m">
+  <?php include 'nav.php'; ?>
+  <?php include 'match-infos.php'; ?>
+  <?php include 'menu.php'; ?>
+</section>
